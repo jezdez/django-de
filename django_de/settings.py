8f7a0e968493785a1419f2d1ad0954d7de1ab0cc
jannis <jannis@72ec9c59-00b0-4840-a13a-91ba528515d4>
2007-12-03 21:22:28 +0000

    gosh :)
    git-svn-id: https://www.django-de.org/svn/django/site/trunk@4 72ec9c59-00b0-4840-a13a-91ba528515d4

--- django_de/settings.py
+++ django_de/settings.py
@@ -9,10 +9,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'    # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'djangode'  # Or path to database file if using sqlite3.
-DATABASE_USER = 'djangode'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'ZCc28WPJH3U63YFW'         # Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'    # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'django_de.db'  # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
