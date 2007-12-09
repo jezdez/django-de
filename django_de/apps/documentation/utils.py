c7e2e9b8365e936f7a4e19f7f1ad15ae37216bff
jannis <jannis@72ec9c59-00b0-4840-a13a-91ba528515d4>
2007-12-09 21:14:29 +0000

    fehler vier
    git-svn-id: https://www.django-de.org/svn/django/site/trunk@45 72ec9c59-00b0-4840-a13a-91ba528515d4

--- django_de/apps/documentation/utils.py
+++ django_de/apps/documentation/utils.py
@@ -11,7 +11,7 @@ def get_svnroot(version, subpath):
     client = pysvn.Client()
 
     if subpath is None:
-        docroot = settings.DJANGO_SVN_ROOT
+        docroot = settings.DOCS_SVN_ROOT
     else:
         if version is None:
             version = "trunk"
@@ -19,7 +19,7 @@ def get_svnroot(version, subpath):
         else:
             rel = get_object_or_404(Release, version=version)
             subpath = os.path.join(rel.repository_path, subpath)
-        docroot = urlparse.urljoin(settings.DJANGO_SVN_ROOT, subpath)
+        docroot = urlparse.urljoin(settings.DOCS_SVN_ROOT, subpath)
 
     try:
         client.info2(docroot, recurse=False)
