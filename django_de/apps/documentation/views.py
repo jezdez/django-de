cd585fb1cd74b19c12f4e7ba4a0e5dded357ddc9
jannis <jannis@72ec9c59-00b0-4840-a13a-91ba528515d4>
2007-12-16 16:23:34 +0000

    try vergessen
    git-svn-id: https://www.django-de.org/svn/django/site/trunk@136 72ec9c59-00b0-4840-a13a-91ba528515d4

--- django_de/apps/documentation/views.py
+++ django_de/apps/documentation/views.py
@@ -27,27 +27,32 @@ def index(request, version=None):
     return render_to_response(template_list, context, RequestContext(request, {}))
 
 def detail(request, slug, version=None):
-    client, version, docroot = _get_svnroot(version, settings.DOCS_SVN_PATH)
-
-    docpath = urlparse.urljoin(docroot, slug+".txt")
     try:
-        name, info = client.info2(docpath)[0]
-    except pysvn.ClientError:
-        raise Http404("Invalid doc: %r (version %r)" % (slug, version))
-        
-    cache_key = "django_de:docs:%s:%s:%s" % (version, slug, info.rev.number)
-    parts = cache.get(cache_key)
-    if parts is None:
-        parts = builder.build_document(client.cat(docpath))
-        cache.set(cache_key, parts, 60*60)
+        import pysvn
+    except ImportError:
+        raise Http404("PySVN not found")
+    else:
+        client, version, docroot = _get_svnroot(version, settings.DOCS_SVN_PATH)
     
-    template_list = ["documentation/%s_detail.html" % version, "documentation/detail.html"]
-    context = {
-        "doc": parts, 
-        "revision": info.rev.number, 
-        "all_versions": Release.objects.all(), 
-        "slug": slug,
-        "version": version,
-        "update_date": datetime.datetime.fromtimestamp(info.last_changed_date),
-    }
-    return render_to_response(template_list, context, RequestContext(request, {}))
+        docpath = urlparse.urljoin(docroot, slug+".txt")
+        try:
+            name, info = client.info2(docpath)[0]
+        except pysvn.ClientError:
+            raise Http404("Invalid doc: %r (version %r)" % (slug, version))
+            
+        cache_key = "django_de:docs:%s:%s:%s" % (version, slug, info.rev.number)
+        parts = cache.get(cache_key)
+        if parts is None:
+            parts = builder.build_document(client.cat(docpath))
+            cache.set(cache_key, parts, 60*60)
+        
+        template_list = ["documentation/%s_detail.html" % version, "documentation/detail.html"]
+        context = {
+            "doc": parts, 
+            "revision": info.rev.number, 
+            "all_versions": Release.objects.all(), 
+            "slug": slug,
+            "version": version,
+            "update_date": datetime.datetime.fromtimestamp(info.last_changed_date),
+        }
+        return render_to_response(template_list, context, RequestContext(request, {}))
