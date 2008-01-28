a3114a8486697260a6068c98319d88cce9c93ea7
jannis <jannis@72ec9c59-00b0-4840-a13a-91ba528515d4>
2008-01-28 16:37:22 +0000

    umstellung von memcached zu static file generator für die dokumentationen mittels svn post-commit hook, yeah.
    git-svn-id: https://www.django-de.org/svn/django/site/trunk@285 72ec9c59-00b0-4840-a13a-91ba528515d4

--- django_de/svn-post-commit.py
+++ django_de/svn-post-commit.py
@@ -8,14 +8,18 @@ from django_de.apps.documentation.models import Release
 from django_de.apps.documentation.views import get_documents
 from django_de.generator import quick_publish, quick_delete, StaticGeneratorException
 
-for release in Release.objects.all():
-    urls = ["%s%s/" % (release.get_absolute_url(), doc) for doc in get_documents(release.version)]
-    try:
-        print "Generating static files.."
-        quick_publish(urls)
-    except StaticGeneratorException, e:
-        print e
+def main():
+    for release in Release.objects.all():
+        urls = ["%s%s/" % (release.get_absolute_url(), doc) for doc in get_documents(release.version)]
+        try:
+            quick_publish(urls)
+        except StaticGeneratorException, e:
+            from django.core.mail import mail_admins
+            mail_admins("Error: SVN commit", e, fail_silently=True)
+            sys.exit(e)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: %s REPOS TXN\n" % (sys.argv[0]))
     else:
-        print "done."
-
-
+        main()
