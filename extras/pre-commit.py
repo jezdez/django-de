3785b9baa1955d735c3d3b324312867dd59f96c8
jannis <jannis@72ec9c59-00b0-4840-a13a-91ba528515d4>
2008-01-28 20:11:18 +0000

    umstellung von memcached zu static file generator für die dokumentationen mittels svn post-commit hook, yeah.
    git-svn-id: https://www.django-de.org/svn/django/site/trunk@292 72ec9c59-00b0-4840-a13a-91ba528515d4

--- extras/pre-commit.py
+++ extras/pre-commit.py
@@ -4,19 +4,11 @@ import sys, os
 sys.path.insert(0, "/home/django-de/lib")
 os.environ['DJANGO_SETTINGS_MODULE'] = "django_de.settings"
 
-from django_de.apps.documentation.models import Release
-from django_de.apps.documentation.views import get_documents
-from django_de.generator import quick_delete, StaticGeneratorException
+from django.dispatch import dispatcher
+from django_de.signals import pre_commit
 
 def main():
-    for release in Release.objects.all():
-        urls = ["%s%s/" % (release.get_absolute_url(), doc) for doc in get_documents(release.version)]
-        try:
-            quick_delete(urls)
-        except StaticGeneratorException, e:
-            from django.core.mail import mail_admins
-            mail_admins("Error: SVN pre commit", e, fail_silently=True)
-            sys.exit(e)
+    dispatcher.send(signal=pre_commit, sender=None, repo=sys.argv[1], rev=sys.argv[2])
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
