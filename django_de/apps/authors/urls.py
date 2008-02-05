63d327fe8d1e34773682bff7680557c8e9e88345
jannis <jannis@72ec9c59-00b0-4840-a13a-91ba528515d4>
2008-02-05 21:46:12 +0000

    kleine änderung an der sortierung der autoren
    git-svn-id: https://www.django-de.org/svn/django/site/trunk@364 72ec9c59-00b0-4840-a13a-91ba528515d4

--- django_de/apps/authors/urls.py
+++ django_de/apps/authors/urls.py
@@ -4,7 +4,7 @@ from django_de.apps.authors.models import Author
 urlpatterns = patterns('django.views.generic.list_detail',
     (r'^$', 'object_list',
         dict(
-            queryset = Author.objects.all(),
+            queryset = Author.objects.order_by('name', 'slug'),
             template_object_name = 'author',
             allow_empty=True,
         ),
