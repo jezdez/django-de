5ddf8dc85ac33c439a279a7fd72b78d7b18f33e3
jannis <jannis@72ec9c59-00b0-4840-a13a-91ba528515d4>
2008-04-17 10:56:47 +0000

    doofer fehler mit falschen import
    git-svn-id: https://www.django-de.org/svn/django/site/trunk@424 72ec9c59-00b0-4840-a13a-91ba528515d4

--- django_de/apps/documentation/management/commands/generatestatic.py
+++ django_de/apps/documentation/management/commands/generatestatic.py
@@ -1,6 +1,7 @@
 from django.core.management.base import NoArgsCommand
 
 from django_de.apps.documentation.utils import get_documents
+
 from django_de.apps.documentation.generator import quick_publish, StaticGeneratorException
 
 class Command(NoArgsCommand):
