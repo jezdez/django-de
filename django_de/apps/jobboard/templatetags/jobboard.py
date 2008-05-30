485ceb138189a009fd1ac42d316c277c0a1944cf
barttc <barttc@72ec9c59-00b0-4840-a13a-91ba528515d4>
2008-05-30 10:07:17 +0000

    Jobbeschreibung auf der Übersichtsseite verkürzt darstellen
    git-svn-id: https://www.django-de.org/svn/django/site/trunk@438 72ec9c59-00b0-4840-a13a-91ba528515d4

--- django_de/apps/jobboard/templatetags/jobboard.py
+++ django_de/apps/jobboard/templatetags/jobboard.py
@@ -3,16 +3,15 @@ from django import template
 from django.template.defaultfilters import stringfilter, mark_safe, striptags, truncatewords
 
 register = template.Library()
-
+    
 @register.filter
 @stringfilter
 def strip_jobdescription(content, max_words=None):
     content = striptags(content)
-    if isinstance(max_words, int) and max_words > 0:
+    if max_words and int(max_words) > 0:
         content = truncatewords(content, max_words)    
     content = markdown.markdown(content)
     return mark_safe(content)
-
 
 @register.filter
 @stringfilter
