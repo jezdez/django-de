50a9bfb2c5aa429860a54ab6543b24f4ba1348f1
Martin Mahner <martin@mahner.org>
2008-08-26 18:48:03 +0200

    ticker app

--- django_de/apps/ticker/urls.py
+++ django_de/apps/ticker/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}, name='ticker_feeds'),
 
     url(r'^archive/$', ticker_views.archive, name='ticker_archive'),
-    url(r'^archive/(?P<tag>[-\w]+)/$$', ticker_views.archive_by_tag, name='ticker_tag_details'),
+    url(r'^archive/(?P<tag>.*)/$', ticker_views.archive_by_tag, name='ticker_archive_details'),
     
     # Details
     url(r'^(?P<id>[\d]+)-(?P<slug>[-\w]+)/$', ticker_views.details, name='ticker_details'),
