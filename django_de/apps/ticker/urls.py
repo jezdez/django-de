20489a9be1f4a953de2dbd06b6f75788de42dddb
Martin Mahner <martin@mahner.org>
2008-09-06 12:17:24 +0200

    Forgot dots in url resolvers.

--- django_de/apps/ticker/urls.py
+++ django_de/apps/ticker/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}, name='ticker_feeds'),
 
     url(r'^archive/$', ticker_views.archive, name='ticker_archive'),
-    url(r'^archive/(?P<tag>[\w\-_ ]+)/$', ticker_views.archive_by_tag, name='ticker_archive_details'),
+    url(r'^archive/(?P<tag>[\w\-_\. ]+)/$', ticker_views.archive_by_tag, name='ticker_archive_details'),
 
     # Details
     url(r'^(?P<id>[\d]+)-(?P<slug>[-\w]+)/$', ticker_views.details, name='ticker_details'),
